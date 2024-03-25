cdedf7582bfd445052b14a663c61305114ceeae7
Stephanie Irish Paladin <stephanieirishpaladin@gmail.com>
2024-03-25 03:50:14 -0600

    Added GetCoursesByFacultyID endpoint

---
+++ src/UR.CoursePlannerBFF.CourseManagerService/Utility/RequirementsExtensions.cs
@@ -0,0 +1,24 @@
+﻿using UR.CoursePlannerBFF.CourseManagerService.Models;
+
+namespace UR.CoursePlannerBFF.CourseManagerService.Utility
+{
+    internal static class RequirementsExtensions
+    {
+        //public IEnumerable<Requirement> PopulateOccurences(this IEnumerable<Requirement> requirements)
+        //{
+        //    IEnumerable<Requirement> result;
+        //    foreach(Requirement requirement in requirements)
+        //    {
+        //        if(requirement.requirement_occurence > 1)
+        //        {
+
+        //        }
+        //        else
+        //        {
+
+        //        }
+
+        //    }
+        //} 
+    }
+}
